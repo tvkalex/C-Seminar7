23af78d11c62535175837b7095cf0ed2f287e5cd
tvkalex <tvkalex@yandex.ru>
2022-12-23 19:18:21 +0300

    Add Seminar7Task1 Solution

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+System.Console.WriteLine("Введите размер массив M x N");
+int m = inputNumber("m = ");
+int n = inputNumber("n = ");
+double[,] arr = new double[m, n];
+PrintArray(FillArray(m, n));
+
+
+int inputNumber(string message)
+{
+    int number;
+    string text;
+    while (true)
+    {
+        Console.Write(message);
+        text = Console.ReadLine()!;
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+    return number;
+}
+
+double[,] FillArray(int m, int n)
+{
+    Random rand = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = Math.Round(rand.Next(-10, 11) + rand.NextDouble(), 2);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write(arr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
