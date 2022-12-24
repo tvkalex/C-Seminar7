b71b245220d610347085a6e5158ea4d236c5539a
tvkalex <tvkalex@yandex.ru>
2022-12-25 01:33:47 +0300

    Add Seminar7Task3 Solution

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее
+// арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+Console.WriteLine("Введите размер массив m x n:");
+int m = inputNumber("m = ");
+int n = inputNumber("n = ");
+int[,] arr = new int[m, n];
+Console.Write("Исходный массив:\n");
+FillArray(arr);
+PrintArray(arr);
+Console.WriteLine("--------------");
+AverageColumnSum(arr);
+Console.WriteLine("\nСреднее арифметическое каждого столбца");
+Console.WriteLine();
+
+void AverageColumnSum(int[,] arr)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        double sum = 0;
+        double averageSum = 0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum += arr[i, j];
+        }
+        averageSum = Math.Round(sum / arr.GetLength(0), 2);
+        Console.Write($"{averageSum}" + "\t");
+    }
+}
+
+int inputNumber(string message)
+{
+    int number;
+    string text;
+    while (true)
+    {
+        Console.Write(message);
+        text = Console.ReadLine()!;
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+    return number;
+}
+
+void FillArray(int[,] array)
+{
+    Random rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write(arr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
